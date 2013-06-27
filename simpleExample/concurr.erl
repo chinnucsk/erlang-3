6d73602757e47c4c82048c68ef9d9eef7282b62e
Administrator <administrator@ubuntu.(none)>
2013-06-27 17:11:04 -0400

    concurrency

---
+++ simpleExample/concurr.erl
@@ -0,0 +1,39 @@
+-module(concurr).
+-export([start/0, say_something/2]).
+-export([start_2/0, ping/2, pong/0]).
+
+say_something(What, 0) ->
+  done;
+say_something(What, Times) ->
+  io:format("~p~n", [What]),
+  say_something(What, Times - 1).
+
+start() ->
+  spawn(concurr, say_something, [hello, 3]),
+  spawn(concurr, say_something, [goodbye, 4]).
+
+ping(0, Pong_pid) ->
+  Pong_pid ! finished,
+  io:format("ping finished~n, []");
+ping(N, Pong_pid) ->
+  Pong_pid ! {ping, self()},
+  receive
+    pong ->
+      io:format("ping received pong~n", [])
+  end,
+  ping(N - 1, Pong_pid).
+
+pong() ->
+  receive 
+    finished -> 
+      io:format("pong finished~n", []);
+    {ping, Ping_pid} ->
+      io:format("pong received ping~n", []),
+      Ping_pid ! pong,
+      pong()
+  end.
+
+start_2() ->
+  Pong_pid = spawn(concurr, pong, []),
+  spawn(concurr, ping, [3, Pong_pid]).
+
--- simpleExample/if_and_case.erl
+++ simpleExample/if_and_case.erl
@@ -1,8 +1,42 @@
 -module(if_and_case).
 -export([convert_length/1]).
+-export([month_length/2]).
 
 convert_length(Length) ->
   case Length of
     {centimeter, X} -> {inch, X / 2.54};
     {inch, Y} -> {centimeter, Y * 2.54}
   end.
+
+
+%Tell the length of a month, given the year and month
+
+month_length(Year, Month) ->
+  Leap = if
+    Year rem 400 == 0 ->
+      leap;
+    trunc(Year / 100) * 100 == Year ->
+      not_leap;
+    trunc(Year / 4) * 4 == Year ->
+      leap;
+    true ->
+      not_leap
+  end,
+
+  case Month of
+    sep -> 30;
+    jan -> 31;
+    mar -> 31;
+    may -> 31;
+    jul -> 31;
+    aug -> 31;
+    oct -> 31;
+    dec -> 31;
+    apr -> 30;
+    jun -> 30;
+    nov -> 30;
+    feb when Leap == leap -> 29;
+    feb -> 28
+  end.
+
+
