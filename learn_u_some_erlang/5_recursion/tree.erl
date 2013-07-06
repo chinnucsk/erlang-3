3f0b402de7cc6c6196c113597babf72997e5aebd
ZhaohuiZh <bravezhaohui@gmail.com>
2013-07-06 19:58:05 -0400

    add lookup function to tree

--- learn_u_some_erlang/5_recursion/tree.erl
+++ learn_u_some_erlang/5_recursion/tree.erl
@@ -14,3 +14,14 @@ insert(NewKey, NewVal, {node, {Key, Val, Smaller, Larger}})
     {node, {Key, Val, Smaller, insert(NewKey, NewVal, Larger)}};
 insert(Key, Val, {node, {Key, _, Smaller, Larger}}) ->
   {node, {Key, Val, Smaller, Larger}}.
+
+loopup({node, 'nil'}, _) ->
+  undefined;
+loopup({node, {Key, Val, _, _}}, Key) ->
+  {ok, Val};
+loopup({node, {Key, _, Smaller, _}}, Target)
+  when Target < Key ->
+    loopup(Smaller, Target);
+loopup({node, {Key, _, _, Larger}}, Target)
+  when Target > Key ->
+    loopup(Larger, Target).
